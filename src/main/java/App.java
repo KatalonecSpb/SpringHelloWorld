a3b87b4d602bbcbbd3eb4edc157ab5875ada4343
KatalonecSPb <anatoluusergeevich@gmail.com>
2023-01-14 22:58:17 +0300

    Change App

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -5,11 +5,11 @@ public class App {
     public static void main(String[] args) {
         ApplicationContext applicationContext = new AnnotationConfigApplicationContext(AppConfig.class);
 
-        HelloWorld bean1 = (HelloWorld) applicationContext.getBean("helloworld");
-        HelloWorld bean2 = (HelloWorld) applicationContext.getBean("helloworld");
+        HelloWorld bean1 = applicationContext.getBean(HelloWorld.class);
+        HelloWorld bean2 = applicationContext.getBean(HelloWorld.class);
 
-        Cat bean3 = (Cat) applicationContext.getBean("cat");
-        Cat bean4 = (Cat) applicationContext.getBean("cat");
+        Cat bean3 = applicationContext.getBean(Cat.class);
+        Cat bean4 = applicationContext.getBean(Cat.class);
 
         System.out.println("Первая пара - " + (bean1 == bean2));
         System.out.println("Вторая пара - " + (bean3 == bean4));
